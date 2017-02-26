fd1b3f0d12f61c7dc22f806c262f95720175ad94
Aleksi Martikainen <aleksi.o.martikainen@student.tut.fi>
2017-02-26 14:23:05 +0200

    Implemented admin site and views for polls. Detail page still WIP

--- web/web/settings.py
+++ web/web/settings.py
@@ -29,8 +29,8 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Helsinki'
 
 USE_I18N = True
 
