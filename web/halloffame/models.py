730ad6b99d5cd5caaed0066dfb96e9d635bad64a
Aleksi O. Martikainen <aleksi.o.martikainen@student.tut.fi>
2018-09-04 20:53:26 +0300

    commented code

--- web/halloffame/models.py
+++ web/halloffame/models.py
@@ -5,6 +5,9 @@ class Chat():
     id = models.CharField(max_length = 255)
     title = models.CharField(max_length=255)
     latestLeet = models.DateTimeField()
+    botUpTimeStart = models.DateTimeField()  # date since last reboot, used for up time calculations
+    messageCount = models.PositiveIntegerField(default=0)
+
 
 
 class TelegramUser():
@@ -16,10 +19,10 @@ class TelegramUser():
     def __str__(self):
         return str(self.id)
 
-
+# Chat members tie the users and chats. TelegramUser can be in many different chats with different ranks.
 class ChatMember():
     chat = models.ForeignKey(Chat, related_name="wat dis?", null=True)
-    tg_user = models.ForeignKey(TelegramUser, related_name="dis wat?", null=True)
+    tg_user = models.ForeignKey(TelegramUser, related_name="dis wat?", null=True)  # is there a way to restrict that only one instanse of a user/chat?
     prestige = models.PositiveIntegerField(default=0)
     rank = models.PositiveIntegerField(default=0)
 
