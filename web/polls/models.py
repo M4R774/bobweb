e5e04437e01c2aa1613c1e70e85bf9f55eb90f07
Aleksi O. Martikainen <aleksi.o.martikainen@student.tut.fi>
2018-11-11 22:33:34 +0200

    remove kebab

--- web/polls/models.py
+++ web/polls/models.py
@@ -1,8 +1,5 @@
-import datetime
-
-from django.db import models
-from django.utils import timezone
 # Create your models here.
+from django.db import models
 
 
 class Question(models.Model):
@@ -19,6 +16,5 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-
     def __str__(self):
         return self.choice_text
