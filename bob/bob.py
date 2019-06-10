dc4486e4fbfa11e731affa1379c205e5e207c871
Aleksi O. Martikainen <aleksi.o.martikainen@student.tut.fi>
2019-06-10 21:31:45 +0300

    Fix bug in prestige system (overindexing)

--- bob/bob.py
+++ bob/bob.py
@@ -9,9 +9,11 @@ import json
 import sys
 import os
 
+# TODO: Logging
+# TODO: Containerization
+# TODO:
 # TODO: xkcd spammer, random or latest?
 # TODO: Reminder upgrades (replys, quotes?)
-# TODO: Speech to text
 # TODO: Periodic proverbs (1 per 1-60 days)
 # TODO: Invent more dank features
 # TODO: weather?
--- bob/message_handler.py
+++ bob/message_handler.py
@@ -99,7 +99,7 @@ def bob_handler(msg, bot):
             print('[INFO] ' + time.strftime("%H:%M:%S") + ' Time and date correct! ')
             bob_chat.latestLeet = date.today()
             bob_chat.save()
-            if sender._rank < len(ranks):
+            if sender._rank < len(ranks) - 1:
                 sender._rank += 1
                 up = u"\U0001F53C"
                 reply = "Asento! " + str(sender.tg_user) + " ansaitsi ylennyksen arvoon " + \
@@ -107,8 +107,7 @@ def bob_handler(msg, bot):
             else:
                 sender.prestige += 1
                 reply = "Asento! " + str(sender.tg_user) + \
-                        " on saavuttanut jo korkeimman mahdollisen sotilasarvon " + \
-                        ranks[sender._rank] + "! Näin ollen " + str(sender.tg_user) + \
+                        " on saavuttanut jo korkeimman mahdollisen sotilasarvon! Näin ollen " + str(sender.tg_user) + \
                         " lähtee uudelle kierrokselle. Onneksi olkoon! " + \
                         "Juuri päättynyt kierros oli hänen " + str(sender.prestige) + ". Lepo. "
                 sender._rank = 0
